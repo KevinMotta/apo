86321ba7037aee2794e7a1da1c9e94b82b97d8a8
KevinMotta <kevinandresmottaparra@gmail.com>
2023-02-18 16:04:12 -0500

    primer comit

---
+++ ejeruno/src/ejeruno/Principal.java
@@ -0,0 +1,9 @@
+package ejeruno;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Student std = new Student();
+		System.out.println(std.stuID);
+	}
+}
---
+++ ejeruno/src/ejeruno/Student.java
@@ -0,0 +1,7 @@
+package ejeruno;
+
+public class Student {
+	String name="Lisa Palombo";
+	double stuId =123456789;
+	String stuStatus="Active";
+}
